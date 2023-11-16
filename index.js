765c834f7d82bf07e4988edd4afdf8fde450840c
Albirozado <albirosaldoalfredo12@gmail.com>
2023-11-16 19:42:24 +0200

    index

--- index.js
+++ index.js
@@ -6,26 +6,27 @@ require("dotenv").config()
 
 
 const app = express()
-const PORT = process.env.PORT || 5000
+const PORT = 5000
+const mongoString = process.env.DATABASE_URL
+app.use(express.json())
+app.use(cors())
 
-const connectDB = async () => {
-    try {
-      const conn = await mongoose.connect(process.env.DATABASE_URL);
-      console.log(`MongoDB Connected: ${conn.connection.host}`);
-    } catch (error) {
-      console.log(error);
-      process.exit(1);
-    }
-  }
-  
-  //Routes go here
-  app.all('/', (req,res) => {
-      res.json({"every thing":"is awesome"})
-  })
+mongoose.connect(mongoString)
+const database = mongoose.connection
+
+database.on("error", (error) =>{
+    console.log(error)
+})
+
+database.once("connected", ()=>{
+    console.log("database connected")
+})
+
+
+
+app.get("/", (req, res) =>{
+    res.send("ola restorante")
+})
 
 app.use("/api", routes)
-connectDB().then(() => {
-    app.listen(PORT, () => {
-        console.log("listening for requests");
-    })
-})
+app.listen(PORT, ()=>{console.log(`servidor corendo na porta ${PORT}`)})
